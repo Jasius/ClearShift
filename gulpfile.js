aff185b03152ad791e83ee88c8ba5debc28b207a
Jaska <jasius@Outlook.com>
2017-03-25 10:34:48 +0200

    ClearShift V4 initial deployment

---
+++ gulpfile.js
@@ -0,0 +1,37 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var minifyCSS = require('gulp-csso');
+var uglify = require('gulp-uglify');
+var livereload = require('gulp-livereload');
+var imagemin = require('gulp-imagemin');
+var pump = require('pump');
+
+gulp.task('sass', function () {
+    gulp.src('./scss/*.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('./dist/css'))
+        .pipe(minifyCSS())
+        .pipe(gulp.dest('./dist/css/minified'))
+        .pipe(livereload());
+});
+gulp.task('compress', function (cb) {
+    pump([
+            gulp.src('js/*.js'),
+            uglify(),
+            gulp.dest('dist/js')
+        ],
+        cb
+    );
+});
+gulp.task('compile', ['sass'], function () {
+    livereload.listen();
+    gulp.watch('./scss/*.scss', ['sass']);
+    gulp.watch('./js/*.js', ['compress']);
+    gulp.watch('./assets/images/*.js', ['images']);
+})
+
+gulp.task('images', () =>
+    gulp.src('assets/images/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('dist/images'))
+);
---
+++ readme.md
@@ -0,0 +1,6 @@
+#ClearShift theme
+![Main theme](https://cloud.githubusercontent.com/assets/11031051/20041578/21267146-a474-11e6-9b2d-34c6f9115a97.png)
+#ClearShift nightmode support
+
+#Seasonal Christmas theme
+![Christmas theme](https://cloud.githubusercontent.com/assets/11031051/21484078/9af37026-cb95-11e6-8900-e5eec7584551.png)
